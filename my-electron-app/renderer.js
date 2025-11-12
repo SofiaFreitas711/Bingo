aa27969aaba5c80582d603a01a3a6a88e4e31fa7
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2025-11-12 15:32:24 +0000

    play, pause and restart are working

--- my-electron-app/renderer.js
+++ my-electron-app/renderer.js
@@ -1,46 +1,77 @@
 // Agora no DOM:
-console.log('✅ renderer carregado')
+// console.log('✅ renderer carregado')
 
-let theme = document.querySelector("select").value
-let select = document.querySelector("select")
-select.addEventListener("change", ()=>{
-    theme = document.querySelector("select").value
-    console.log(theme);
-    
-})
-
-console.log(theme)
-
-let nums = []
-let numsOut = []
-for(let i=1; i<100;i++){
-    nums.push(i)
-}
-console.log(nums);
-
-// let con = true
-let time = 500
-    
-let interval = setInterval(()=>{
-        let len = nums.length
-        // console.log(len);
+window.addEventListener("DOMContentLoaded",()=>{
+    let theme = document.querySelector("select").value
+    let select = document.querySelector("select")
+    select.addEventListener("change", ()=>{
+        theme = document.querySelector("select").value
+        console.log(theme);
         
-        let selected = Math.floor(Math.random() * len)
-        // console.log(selected)
-        let num = nums[selected]
-        // console.log(num);
-        numsOut.unshift(num)
-        console.log(numsOut);
-        
-        nums = nums.filter((a)=>a != num)
-        if(nums.length == 0){
-            // console.log("entrei aqui");
+    })
+
+    console.log(theme)
+
+    let nums = []
+    let numsOut = []
+    for(let i=1; i<100;i++){
+        nums.push(i)
+    }
+    console.log(nums);
+
+
+    let interval
+    let time = 2000
+
+    // function for bingo run
+    function intervalFun(){
+            let len = nums.length
+            // console.log(len);
             
-            clearInterval(interval)
-        }
-        // console.log(nums.length);
+            let selected = Math.floor(Math.random() * len)
+            // console.log(selected)
+            let num = nums[selected]
+            // console.log(num);
+            numsOut.unshift(num)
+            console.log(numsOut);
+            
+            nums = nums.filter((a)=>a != num)
+            if(nums.length == 0){
+                // console.log("entrei aqui");
+                
+                clearInterval(interval)
+            }
 
+    }
+
+    // stop bingo
+    function stop(){
+        clearInterval(interval)
+        console.log("parou");
         
-    },time)
 
+    }
+
+    // start bingo
+    function start(){
+        interval = setInterval(intervalFun,time)
+        console.log("começou");
+    }
+
+    function restart(){
+        nums = []
+        numsOut = []
+        for(let i=1; i<100;i++){
+            nums.push(i)
+        }
+    }
+    
+
+    let startBtn = document.querySelector("#start")
+    startBtn.addEventListener("click", start)
+    let stopBtn = document.querySelector("#stop")
+    stopBtn.addEventListener("click",stop)
+    let restartBtn = document.querySelector("#restart")
+    restartBtn.addEventListener("click",restart)
+})
 
